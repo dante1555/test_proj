4eeb26fbb3678d40e46d3b72f9588355d39a8591
dante1555 <dante1555@yandex.ru>
2016-12-15 23:29:22 +0300

    norm animation

--- Assets/SpriteSheet.cs
+++ Assets/SpriteSheet.cs
@@ -3,40 +3,63 @@ using System.Collections;
 
 public class SpriteSheet : MonoBehaviour
 {
-    public int _uvTieX = 1;
-    public int _uvTieY = 1;
-    public int _fps = 10;
+	public int Columns = 5;
+	public int Rows = 5;
+	public float FramesPerSecond = 10f;
+	public bool RunOnce = true;
 
-    private Vector2 _size;
-    private Renderer _myRenderer;
-    private int _lastIndex = -1;
+	public float RunTimeInSeconds
+	{
+		get
+		{
+			return ( (1f / FramesPerSecond) * (Columns * Rows) );
+		}
+	}
 
-    void Start()
-    {
-        _size = new Vector2(1.0f / _uvTieX, 1.0f / _uvTieY);
-        _myRenderer = GetComponent<Renderer>();
-        if (_myRenderer == null)
-            enabled = false;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        // Calculate index
-        int index = (int)(Time.timeSinceLevelLoad * _fps) % (_uvTieX * _uvTieY);
-        if (index != _lastIndex)
-        {
-            // split into horizontal and vertical index
-            int uIndex = index % _uvTieX;
-            int vIndex = index / _uvTieY;
+	private Material materialCopy = null;
 
-            // build offset
-            // v coordinate is the bottom of the image in opengl so we need to invert.
-            Vector2 offset = new Vector2(uIndex * _size.x, 1.0f - _size.y - vIndex * _size.y);
+	void Start()
+	{
+		// Copy its material to itself in order to create an instance not connected to any other
+		materialCopy = new Material(GetComponent<Renderer>().sharedMaterial);
+		GetComponent<Renderer>().sharedMaterial = materialCopy;
 
-            _myRenderer.material.SetTextureOffset("_MainTex", offset);
-            _myRenderer.material.SetTextureScale("_MainTex", _size);
+		Vector2 size = new Vector2(1f / Columns, 1f / Rows);
+		GetComponent<Renderer>().sharedMaterial.SetTextureScale("_MainTex", size);
+	}
 
-            _lastIndex = index;
-        }
-    }
+	void OnEnable()
+	{
+		StartCoroutine(UpdateTiling());
+	}
+
+	private IEnumerator UpdateTiling()
+	{
+		float x = 0f;
+		float y = 0f;
+		Vector2 offset = Vector2.zero;
+
+		while (true)
+		{
+			for (int i = Rows-1; i >= 0; i--) // y
+			{
+				y = (float) i / Rows;
+
+				for (int j = 0; j <= Columns-1; j++) // x
+				{
+					x = (float) j / Columns;
+
+					offset.Set(x, y);
+
+					GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+					yield return new WaitForSeconds(1f / FramesPerSecond);
+				}
+			}
+
+			if (RunOnce)
+			{
+				yield break;
+			}
+		}
+	}
 }
